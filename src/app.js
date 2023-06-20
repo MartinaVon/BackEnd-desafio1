76fe3844ff3499cbad6f9bae3cad65651e86443f
Martina VW <martivonwernich@gmail.com>
2023-06-19 21:30:32 -0300

    a

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+    res.send("Hola mundo")
+})
+
+app.listen(port, () => console.log("levantando puerto 3000"))
